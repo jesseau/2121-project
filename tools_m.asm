f354c1e1e221d83e91a9b2b74ebf35ac16349b2e
Jesse <jessez594@gmail.com>
2015-05-30 21:48:43 +1000

    split macros and functions
    Split macros and functions into separate files. "m" denotes macros and
    other header definitions, "f" denotes functions.

--- tools_m.asm
+++ tools_m.asm
@@ -85,49 +85,6 @@ tools_result:
 	mov @0, @2
 .endmacro
 
-
 .equ F_CPU = 16000000
 .equ DELAY_1MS = F_CPU / 4 / 1000 - 4
 ; 4 cycles per iteration - setup/call-return overhead
-
-sleep_1ms:
-	pushall	
-	ldi r25, high(DELAY_1MS)
-	ldi r24, low(DELAY_1MS)
-delayloop_1ms:
-	sbiw r25:r24, 1
-	brne delayloop_1ms
-	popall
-	ret
-
-sleep_5ms:
-	rcall sleep_1ms
-	rcall sleep_1ms
-	rcall sleep_1ms
-	rcall sleep_1ms
-	rcall sleep_1ms
-	ret
-
-sleep_20ms:
-	rcall sleep_5ms
-	rcall sleep_5ms
-	rcall sleep_5ms
-	rcall sleep_5ms
-	ret
-
-sleep_100ms:
-	rcall sleep_20ms
-	rcall sleep_20ms
-	rcall sleep_20ms
-	rcall sleep_20ms
-	rcall sleep_20ms
-	ret
-
-sleep_500ms:
-	rcall sleep_100ms
-	rcall sleep_100ms
-	rcall sleep_100ms
-	rcall sleep_100ms
-	rcall sleep_100ms
-	ret
-
